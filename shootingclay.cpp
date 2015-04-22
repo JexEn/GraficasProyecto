43fa9a30220acd366c4cc658e8fbf0b70e6330c3
JexEn <ghx1337@gmail.com>
2015-04-22 17:16:42 -0500

    Textura de Fondo; Marcador de score (creo); efecto de sonido al hacer click; luces tronaron todo, corregir (A)

--- shootingclay.cpp
+++ shootingclay.cpp
@@ -30,6 +30,7 @@
 #include <math.h>
 #include <time.h>
 #include <mmsystem.h>
+#include "imageloader.h"
 
 float x=1.0;
 using namespace std;
@@ -41,19 +42,22 @@ char reset[7] = {'R','-','R','E','S','E','T'};
 char eexit[8] = {'E','S','C','-','E','X','I','T'};
 char miNombre[10] = {'J','a','i','m','e',' ','N','e','r','i'};
 char miMatricula[9] = {'A','0','1','0','3','4','1','1','2'};
-char marcadorIzq[200] = "";
-char marcadorDer[200] = "";
+char marcadorPuntaje[200] = "";
 
 float velocidadDeDificultad = 80;
 bool ammunition = false;
 int balasRestantes = 3;
 int rasterBalas = 395;
+int puntaje = 1000000;
+
+static GLuint texName[36];
 
 //variables de movimiento para un disco
 float tiempoTranscurrido = 0;
 float traslacionDiscoUnoX = 0;
-float traslacionDiscoUnoY = 0;
-float traslacionDiscoUnoZ = 60;
+float traslacionDiscoUnoY = 10;
+float traslacionDiscoUnoZ = -20;
+int anguloDeTiro = 0;
 bool discoUnoEnPantalla = false;
 
 bool cronometro = false;
@@ -81,6 +85,80 @@ void init(void){
     //PlaySound("C:\\Users\\JNeri\\Documents\\Graficas\\Semana 6\\Tarea_Pong\\trophy_rush.wav", NULL, SND_SYNC|SND_FILENAME|SND_LOOP);
 }
 
+void loadTexture(Image* image,int k){
+
+    glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
+                 0,                            //0 for now
+                 GL_RGB,                       //Format OpenGL uses for image
+                 image->width, image->height,  //Width and height
+                 0,                            //The border of the image
+                 GL_RGB, //GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
+                 //as unsigned numbers
+                 image->pixels);               //The actual pixel data
+
+}
+
+
+
+void initRendering(){
+    GLuint i=0;
+       GLfloat ambientLight[] = {0.2f, 0.2f, 0.2f, 1.0f};
+    //glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+
+    GLfloat directedLight[] = {0.9f, 0.9f, 0.9f, 1.0f};
+    GLfloat directedLightPos[] = {-10.0f, 15.0f, 20.0f, 0.0f};
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, directedLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, directedLightPos);
+    //glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    //glEnable(GL_NORMALIZE); ///Users/mariaroque/Imagenes
+
+    glEnable(GL_TEXTURE_2D);
+
+    glEnable(GL_COLOR_MATERIAL);
+    glGenTextures(26, texName); //Make room for our texture
+    Image* image;
+
+    /* = loadBMP("/Users/mariaroque/Imagenes/img01.bmp");
+    loadTexture(image,0);
+
+    image = loadBMP("/Users/mariaroque/Imagenes/img02.bmp");
+    loadTexture(image,1);
+
+    image = loadBMP("/Users/mariaroque/Imagenes/img03.bmp");
+    loadTexture(image,2);
+
+    image = loadBMP("/Users/mariaroque/Imagenes/img04.bmp");
+    loadTexture(image,3);
+
+    image = loadBMP("/Users/mariaroque/Imagenes/img05.bmp");
+    loadTexture(image,4);
+
+    image = loadBMP("/Users/mariaroque/Imagenes/img06.bmp");
+    loadTexture(image,5);*/
+
+    image = loadBMP("C:\\Users\\JNeri\\GraficasProyecto\\imagenes\\background.bmp");loadTexture(image,i++);
+
+    // image = loadBMP("C:\\Users\\JNeri\\GraficasProyecto\\imagenes\\bandera argentina.bmp");loadTexture(image,i++);
+    // image = loadBMP("C:\\Users\\JNeri\\GraficasProyecto\\imagenes\\bandera brasil.bmp");loadTexture(image,i++);
+    // image = loadBMP("C:\\Users\\JNeri\\GraficasProyecto\\imagenes\\bandera canada.bmp");loadTexture(image,i++);
+
+    delete image;
+}
 
 
 void discos (int v){
@@ -99,7 +177,14 @@ void discos (int v){
         if(v == 1){
             traslacionDiscoUnoZ = velocidadDeDificultad * tiempoTranscurrido * cos(45);
             traslacionDiscoUnoY = (velocidadDeDificultad * tiempoTranscurrido * sin(45)) - (4.9 * pow(tiempoTranscurrido, 2));
-            traslacionDiscoUnoX += 1;
+
+            if(traslacionDiscoUnoX > 0){
+                traslacionDiscoUnoX -= 1;
+            }else if(traslacionDiscoUnoX < 0){
+                traslacionDiscoUnoX += 1;
+            }else
+                traslacionDiscoUnoX+=0;
+
             tiempoTranscurrido += 0.02;
 
 
@@ -109,6 +194,9 @@ void discos (int v){
             if(traslacionDiscoUnoZ > 1400 || traslacionDiscoUnoY < 0){
                 desaparece = true;
                 discoUnoEnPantalla = false;
+                srand(time(NULL));
+                traslacionDiscoUnoX = rand() % 400;
+                anguloDeTiro = rand()%30+20;
 
             }
 
@@ -121,10 +209,6 @@ void discos (int v){
 
 }
 
-/*void discoDos (int v){
-
-
-}*/
 
 void draw3dString (void *font, char *s, float x, float y, float z){
     unsigned int i;
@@ -270,6 +354,41 @@ void gameArea() {
         glColor3d(1,1,1);
         sprintf(buffer, "S H O T S");
         draw3dStringScale(GLUT_STROKE_MONO_ROMAN, 0.1, buffer, -400, 1, 0);
+
+
+        //Textura / Fondo, para que no se vea tan pirata el area de juego...
+        glBindTexture(GL_TEXTURE_2D, texName[0]);
+        glPushMatrix();
+        glScalef(14.5,14,1.0);
+        glTranslated(0,0,100);
+        glBegin(GL_QUADS);
+            glTexCoord3f(0.0f, 0.0f, -1500.0f);
+            glVertex3f(-420.0f, 0.0f, -1500.0f);
+
+            glTexCoord3f(1.0f, 0.0f, -1500.0f);
+            glVertex3f(420.0f, 0.0f, -1500.0f);
+
+            glTexCoord3f(1.0f, 1.0f, -1500.0f);
+            glVertex3f(420.0f, 220.0f, -1500.0f);
+
+            glTexCoord3f(0.0f, 1.0f, -1500.0f);
+            glVertex3f(-420.0f, 220.0f, -1500.0f);
+        glEnd();
+        glPopMatrix();
+
+        //Scoreboard
+        glColor3d(1,1,1);
+        sprintf(marcadorPuntaje,"%1d",puntaje);
+        draw3dStringScale(GLUT_STROKE_MONO_ROMAN, 0.07, marcadorPuntaje, 330, 10, 0);
+        sprintf(buffer, "S C O R E");
+        draw3dStringScale(GLUT_STROKE_MONO_ROMAN, 0.07, buffer, 320, 1, 0);
+
+        glColor3d(0,0,0);
+        glPushMatrix();
+        //glTranslated(0,0,1);
+        glRectd(300,0,403,18);
+        glPopMatrix();
+
     }
 
     // Dibujar discos; el dibujado es independiente uno del otro
@@ -316,6 +435,7 @@ void keyboard(unsigned char key, int mouseX, int mouseY){
         case 'r':
         case 'R':
             tiempoTranscurrido = 0;
+            velocidadDeDificultad = 80;
             balasRestantes = 3;
             aparecerNombres = true;
             newGame = false;
@@ -323,13 +443,14 @@ void keyboard(unsigned char key, int mouseX, int mouseY){
             difficultyText = false;
             desaparece = true;
             traslacionDiscoUnoX = 0;
+
             // cronometro = false;
             glutPostRedisplay();
             break;
         case 'i':
         case 'I':
-            desaparece = false;
-            glutTimerFunc(1, discos, 1);
+            // desaparece = false;
+            // glutTimerFunc(1, discos, 1);
             glutPostRedisplay();
             break;
         case 13:
@@ -338,17 +459,20 @@ void keyboard(unsigned char key, int mouseX, int mouseY){
                 newGame = true;
                 ammunition = true;
                 discoUnoEnPantalla = true;
+                desaparece = false;
                 glutTimerFunc(1000, discos, 1);
             }else if(triangleRasterY == 125.0){ //La dificultad seleccionada es Easy
                 newGame = true;
                 ammunition = true;
                 discoUnoEnPantalla = true;
+                desaparece = false;
                 velocidadDeDificultad = (3*velocidadDeDificultad)/4;
                 glutTimerFunc(1000, discos, 1);
             } else if(triangleRasterY == 75.0){ //La dificultad seleccionada es Hard
                 newGame = true;
                 ammunition = true;
                 discoUnoEnPantalla = true;
+                desaparece = false;
                 velocidadDeDificultad = velocidadDeDificultad * 2.5;
                 glutTimerFunc(1000, discos, 1);
             }
@@ -394,8 +518,10 @@ void dispara(int button, int state, int mouseX, int mouseY){
         /* left button increase joint angle, right button decreases it */
 
         //Si el usuario oprime el boton izq del mouse
-          if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)//{
+          if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
             balasRestantes-=1;
+            PlaySound("C:\\Users\\JNeri\\GraficasProyecto\\shotgun.wav", NULL, SND_ASYNC|SND_FILENAME);
+        }
     //         expuesta[posicionX] = true;
     //         dibujaBaraja();
     //         if(contExpuestas==0){
@@ -450,18 +576,233 @@ int main(int argc, char** argv){
     glutInitWindowSize (800, 600);
     glutInitWindowPosition (100, 100);
     glutCreateWindow (argv[0]);
-    init ();
-    glEnable(GL_DEPTH_TEST); //para diferenciar que vertices estan al frente y detras ver ejemplo del documento de word
-    glutDisplayFunc(gameArea);
-//    glutTimerFunc(25, myTimer, 1);
 
+    init();
+    initRendering();
+    glEnable(GL_DEPTH_TEST); //para diferenciar que vertices estan al frente y detras ver ejemplo del documento de word
+
+    glutDisplayFunc(gameArea);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
     glutSpecialFunc(specialKeys);
     glutMouseFunc(dispara);
 
-    // creacionMenu();
 
     glutMainLoop();
     return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <assert.h>
+#include <fstream>
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h)
+{
+
+}
+
+Image::~Image()
+{
+    delete[] pixels;
+}
+
+namespace
+{
+//Converts a four-character array to an integer, using little-endian form
+    int toInt(const char* bytes)
+    {
+        return (int)(((unsigned char)bytes[3] << 24) |
+                     ((unsigned char)bytes[2] << 16) |
+                     ((unsigned char)bytes[1] << 8) |
+                     (unsigned char)bytes[0]);
+    }
+
+//Converts a two-character array to a short, using little-endian form
+    short toShort(const char* bytes)
+    {
+        return (short)(((unsigned char)bytes[1] << 8) |
+                       (unsigned char)bytes[0]);
+    }
+
+//Reads the next four bytes as an integer, using little-endian form
+    int readInt(ifstream &input)
+    {
+        char buffer[4];
+        input.read(buffer, 4);
+        return toInt(buffer);
+    }
+
+//Reads the next two bytes as a short, using little-endian form
+    short readShort(ifstream &input)
+    {
+        char buffer[2];
+        input.read(buffer, 2);
+        return toShort(buffer);
+    }
+
+//Just like auto_ptr, but for arrays
+    template<class T>
+    class auto_array
+    {
+    private:
+        T* array;
+        mutable bool isReleased;
+    public:
+        explicit auto_array(T* array_ = NULL) :
+                array(array_), isReleased(false)
+        {
+        }
+
+        auto_array(const auto_array<T> &aarray)
+        {
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+
+        ~auto_array()
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+        }
+
+        T* get() const
+        {
+            return array;
+        }
+
+        T &operator*() const
+        {
+            return *array;
+        }
+
+        void operator=(const auto_array<T> &aarray)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = aarray.array;
+            isReleased = aarray.isReleased;
+            aarray.isReleased = true;
+        }
+
+        T* operator->() const
+        {
+            return array;
+        }
+
+        T* release()
+        {
+            isReleased = true;
+            return array;
+        }
+
+        void reset(T* array_ = NULL)
+        {
+            if (!isReleased && array != NULL)
+            {
+                delete[] array;
+            }
+            array = array_;
+        }
+
+        T* operator+(int i)
+        {
+            return array + i;
+        }
+
+        T &operator[](int i)
+        {
+            return array[i];
+        }
+    };
+}
+
+Image* loadBMP(const char* filename)
+{
+    ifstream input;
+    input.open(filename, ifstream::binary);
+    assert(!input.fail() || !"Could not find file");
+    char buffer[2];
+    input.read(buffer, 2);
+    assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
+    input.ignore(8);
+    int dataOffset = readInt(input);
+
+    //Read the header
+    int headerSize = readInt(input);
+    int width;
+    int height;
+    switch (headerSize)
+    {
+    case 40:
+        //V3
+        width = readInt(input);
+        height = readInt(input);
+        input.ignore(2);
+        assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+        assert(readShort(input) == 0 || !"Image is compressed");
+        break;
+    case 12:
+        //OS/2 V1
+        width = readShort(input);
+        height = readShort(input);
+        input.ignore(2);
+        assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+        break;
+    case 64:
+        //OS/2 V2
+        assert(!"Can't load OS/2 V2 bitmaps");
+        break;
+    case 108:
+        //Windows V4
+        assert(!"Can't load Windows V4 bitmaps");
+        break;
+    case 124:
+        //Windows V5
+        assert(!"Can't load Windows V5 bitmaps");
+        break;
+    default:
+        assert(!"Unknown bitmap format");
+    }
+
+    //Read the data
+    int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+    int size = bytesPerRow * height;
+    auto_array<char> pixels(new char[size]);
+    input.seekg(dataOffset, ios_base::beg);
+    input.read(pixels.get(), size);
+
+    //Get the data into the right format
+    auto_array<char> pixels2(new char[width * height * 3]);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                pixels2[3 * (width * y + x) + c] =
+                    pixels[bytesPerRow * y + 3 * x + (2 - c)];
+            }
+        }
+    }
+
+    input.close();
+    return new Image(pixels2.release(), width, height);
 }
